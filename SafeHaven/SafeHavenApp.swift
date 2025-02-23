311f11a5b05e91e2215a6f76b872af7c57106a8e
Travis Rodriguez <travisgolembiewski@gmail.com>
2025-02-23 16:43:08 -0500

    Initial Commit

---
+++ SafeHaven/SafeHavenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeHavenApp.swift
+//  SafeHaven
+//
+//  Created by Travis Rodriguez on 2/23/25.
+//
+
+import SwiftUI
+
+@main
+struct SafeHavenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SafeHavenTests/SafeHavenTests.swift
@@ -0,0 +1,17 @@
+//
+//  SafeHavenTests.swift
+//  SafeHavenTests
+//
+//  Created by Travis Rodriguez on 2/23/25.
+//
+
+import Testing
+@testable import SafeHaven
+
+struct SafeHavenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
